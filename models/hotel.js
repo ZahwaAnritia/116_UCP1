78f3de38cd46d2751d072e879ce4d5a5210bd701
ZahwaAnritia <anritia15@gmail.com>
2025-11-05 10:17:47 +0700

    menambahkan file hotel.js

--- models/hotel.js
+++ models/hotel.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     Tanggal_Pesan: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.DATETIME,
       allowNull: false
     }
   }, {
